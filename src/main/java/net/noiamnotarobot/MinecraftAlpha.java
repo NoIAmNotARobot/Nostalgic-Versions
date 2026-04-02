e8777c0d971d8515a8b4c425b274d27975ecc7c1
NoIAmNotARobot <237332689+NoIAmNotARobot@users.noreply.github.com>
2026-04-02 19:33:11 -0400

    Initial commit

---
+++ src/main/java/net/noiamnotarobot/MinecraftAlpha.java
@@ -0,0 +1,17 @@
+package net.noiamnotarobot;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MinecraftAlpha implements ModInitializer {
+	public static final String MOD_ID = "minecraft-alpha";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Initializing {}...", MOD_ID);
+        LOGGER.info("Initialized {}!", MOD_ID);
+	}
+}
